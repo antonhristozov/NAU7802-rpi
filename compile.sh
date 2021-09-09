9ed1804a64fe009b7f278ed5d6849b387248b0db
Anton Hristozov <anton.histozov@gmail.com>
2021-09-10 00:41:16 +0100

    Updated compile.sh to include new executable

--- compile.sh
+++ compile.sh
@@ -2,3 +2,5 @@
 echo "Creating executables:"
 gcc -Wall -o load NAU7802_driver.c NAU7802.c -lwiringPi -lm
 gcc -Wall -o test test.c NAU7802.c -lwiringPi
+gcc -Wall -o TestSensorfunctions TestSensorFunctions.c SensorFunctions.c NAU7802.c -lwiringPi -lm
+
