c9d4d2770fbb30426ccf85742df0fe144dffe01f
Anton Hristozov <anton.histozov@gmail.com>
2021-09-20 15:33:27 +0100

    hx711.c

--- hx711.c
+++ hx711.c
@@ -3,16 +3,25 @@
 #include "NAU7802.h"
 #include "SensorFunctions.h"
 
+static struct load_cal lc;
 
 int hx711_initialize(void){
    int fd = -1;
    printf("Initializing sensor\n\n");
    fd = init_sensor();
    calibrate_sensor(fd);
+   NAU7802_init_load_cal(&lc);
+   NAU7802_setLoadCalGain(&lc, 0.25);
+   NAU7802_setShiftLoad(&lc, 0);
+   NAU7802_calibrate(fd, CALMOD_GCS);
+   NAU7802_getLinearLoad(fd, &lc);
+   delay(500);
+   NAU7802_tareLoad(fd, &lc);
    return fd;
 }
 
 double hx711_read_sensor_data(void){
+   const int SHIFT4 = 4;
    static int first_call = 0;
    static int fd = 0;
    double load_value = 0.0;
@@ -20,7 +29,8 @@ double hx711_read_sensor_data(void){
 	fd = hx711_initialize();
         first_call = 1;
    }
-   load_value = read_load(fd);
+   load_value = NAU7802_readADCS(fd,SHIFT4); 
+   load_value = NAU7802_getLinearLoad(fd, &lc);
    load_value = convert_to_kilograms(load_value);
    /* Place value in shared memory */
    return load_value;
